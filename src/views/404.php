e0ff52b02f2c8242c0a6e8efc9baba6bb03f9e9d
Patriach Cain <youanybluesky10@gmail.com>
2024-09-14 23:26:41 +0800

    fixed 404 page

--- src/views/404.php
+++ src/views/404.php
@@ -1,17 +1,18 @@
 <?php
 
+$header_info = [
+    "trace" => parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
+    "title" => "404 Error: Page Not Found",
+    "description" => "",
+];
+
 require "partials/head.php";
 require "partials/background.php";
 require "partials/header.php";
 require "partials/nav.php";
+require "partials/main.php";
 ?>
-
-    <div class="w-full h-full p-4 flex flex-col">
-        <h1 class="text-lg font-bold mt-2 2xl:text-2xl">404 Error: Page Not Found</h1>
-        <hr class="border border-[#61a0ff] w-full">
-        <a href="/home" class="text-blue-500 underline">go back</a>
-    </div>
-
+    <a href="/home" class="text-blue-500 underline">go back</a>
 <?php
 require "partials/aside.php";
 require "partials/footer.php";
