c37199fe833a4171ed5cb603accf8a76f3a38d84
Patriach Cain <youanybluesky10@gmail.com>
2024-09-14 09:28:09 +0800

    fix minor bugs

--- src/views/partials/head.php
+++ src/views/partials/head.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="images/icon.svg">
+    <link rel="icon" type="image/x-icon" href="images/icon.png">
     <link rel="stylesheet" href="dist/style.css">
     <title>Shimi's Room</title>
 </head>
--- src/views/partials/main.php
+++ src/views/partials/main.php
@@ -2,7 +2,7 @@
     2xl:grid-cols-[75%_25%]
     lg:w-1/2 2xl:w-1/2">
 
-    <div class="w-full h-full p-4 flex flex-col">
+    <div id="projects_div" class="w-full overflow-auto h-full p-4 flex flex-col">
         <div class="w-full flex justify-between">
             <h1 class="text-lg font-bold mt-2 2xl:text-2xl"><?= $header_info['title'] ?? "u r lost😭" ?></h1>
             <p id="timer" class="text-lg font-bold mt-2 2xl:text-2xl hidden">00:00:00</p>
