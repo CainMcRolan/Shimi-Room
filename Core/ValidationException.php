8341c59ead1aafbf01cb68c7b6cdff9558a4ade3
Patriach Cain <youanybluesky10@gmail.com>
2024-10-04 12:54:25 +0800

    added comment

--- Core/ValidationException.php
+++ Core/ValidationException.php
@@ -7,6 +7,9 @@ class ValidationException extends \Exception
     public readonly array $errors;
     public readonly array $old;
 
+    /**
+     * @throws ValidationException
+     */
     public static function throw($errors, $old)
     {
         $instance = new static();
